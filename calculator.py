f3850277cc8812a8b6b5a56911deb49cff66b630
P-LINKS <120776072+P-LINKS@users.noreply.github.com>
2023-11-14 22:34:31 +0500

    Update calculator.py
    calculator algorithm

--- calculator.py
+++ calculator.py
@@ -1,1 +1,25 @@
-#calculator code start from  calculation function 
+#calculator code start from  calculation function
+def calculator():
+    x = float(input("Enter 1st value  "))
+    operater= input("please select operater (+,-,*,/)")
+    y = float(input("Enter2nd Value "))
+
+#define operater function
+    if operator ='+':
+      result = X + y
+    elif operator ='-':
+      result = x- y
+    elif operator = '*':
+      result = x * y
+    elif operator = '/':
+      if y !=0:
+        result =x / y
+      else:
+        print( Error; divisional error )
+        return none
+    else:
+      print(invalid input )
+
+result=calculator()
+
+print(result)
